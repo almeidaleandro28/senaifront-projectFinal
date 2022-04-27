1c7698f056ea99952af606548d3eba58f838a514
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-04-27 19:24:04 -0300

    clone card

--- src/Assets/products/index.js
+++ src/Assets/products/index.js
@@ -1,3 +1,24 @@
+let products = [];
+
+function getProducts() {
+  const request = new XMLHttpRequest();
+  request.open("GET", "src/Assets/products/products.json");
+  request.onload = function() {
+    products = JSON.parse(this.responseText);
+    console.log( products)
+  }
+  request.send();
+}
+
+function generateCard() {
+  const card = document.querySelector(".card").cloneNode(true);
+  card.querySelector(".card-content > h2").innerHTML = "odlsldldld";
+  document.querySelector(".section").append(card);
+
+
+
+}
+
 const bt_add = document.getElementById("bt-sum");
 const bt_decrease = document.getElementById("bt-sub");
 let counter = 0;
@@ -15,3 +36,5 @@ function sub() {
 function updateAmount( counter ) {
   document.getElementById("price").value = counter;
 }
+
+generateCard();
